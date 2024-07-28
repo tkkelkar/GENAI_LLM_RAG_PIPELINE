3b2dafd29b1b9e492c9d01249e02e090ead8edcc
Tarun <47100776+tkkelkar@users.noreply.github.com>
2024-07-28 17:08:12 +0530

    Add files via upload

---
+++ deploy_llm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source .env
+
+# deploy model
+docker run  --rm --name llamacpp-container \
+-p 8080:8080 \
+-v $MODEL_DIR:/models \
+ghcr.io/ggerganov/llama.cpp:server \
+-m /models/${MODEL_PATH} \
+--ctx-size 8192 \
+--cont-batching \
+--embeddings \
+--host 0.0.0.0 \
+--port 8080
+
+# -m /models/models-gguf/$MODEL_PATH \
